7ef5ee2ffceb97f56664b70c8ddf496bebc8c6a7
developer <akrem.abayed@gmail.com>
2015-05-20 16:37:55 +0200

    1722

--- test/maggaMediator/hooks.test.js
+++ test/maggaMediator/hooks.test.js
@@ -1,11 +1,10 @@
 'use strict';
 var MaggaMediator = require('maggaMediator.js');
 
-describe.only('hooks', function () {
+describe('hooks', function () {
   MaggaMediator = new MaggaMediator();
   MaggaMediator.subscribe('channel:one', function () {
     console.log("smthg published");
   });
   MaggaMediator.publish('channel:one', 'magic data');
-
 });

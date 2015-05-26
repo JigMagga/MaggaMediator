90ae0a39d27d10a36d005b355e25ca265283e9cc
Jaroslav Popov <jaroslav.popov@takeaway.com>
2015-05-26 09:26:26 +0200

    eslint styling

--- plugins/baconjs.js
+++ plugins/baconjs.js
@@ -1,75 +1,72 @@
-var Bacon = require("baconjs").Bacon;
+var Bacon = require('baconjs').Bacon;
 
 module.exports = {
-    init: function(mediator){
+    init: function (mediator) {
         mediator.bacon = {
             bus: new Bacon.Bus(),
-            event:{}
+            event: {}
         };
-        mediator.getEventStream = function(eventName){
-            var event;
+        mediator.getEventStream = function (eventName) {
+            var event, result;
             // If we don't specify the event then return Bus with all events
             if (typeof eventName === 'undefined') {
-                return this.bacon.bus;
-            }
-            else {
+                result = this.bacon.bus;
+            } else {
                 event = this.bacon.event[eventName];
+                if (typeof event !== 'undefined') {
+                    result = event._eventStream;
+                } else {
+                    // if we don't have such event then we return ended EventStream
+                    result = Bacon.never();
+                }
             }
-            if (typeof event !== 'undefined') {
-                return event._eventStream;
-            }
-            // if we don't have such event then we return ended EventStream
-            else {
-                return Bacon.never();
-            }
-
+            return result;
         };
     },
     subscribe: function (eventName, cb) {
-        var bacon = this.bacon;
+        var bacon = this.bacon,
+            event;
         // If we don't have this event then create it
-        if (typeof bacon.event[eventName] == 'undefined') {
+        if (typeof bacon.event[eventName] === 'undefined') {
             bacon.event[eventName] = {
                 _eventStream:
-                    Bacon.fromEvent(this,'publish',function(eventEventName,eventValue){
+                    Bacon.fromEvent(this, 'publish', function (eventEventName, eventValue) {
                         // transforming to one object
                         // We need event to filter it
                         // Then we will take only value
                         return {
-                            event:eventEventName,
-                            value:eventValue
+                            event: eventEventName,
+                            value: eventValue
                         };
                     })
-                        .filter(function(event){
-                            return event.event === eventName;
-                        }).map(".value") //we need only value
+                        .filter(function (e) {
+                            return e.event === eventName;
+                        }).map('.value')
+                        // we need only value
                         .name(eventName),
                 _unsubscribers: {}
-
             };
             bacon.bus.plug(bacon.event[eventName]._eventStream);
         }
 
-        var event = bacon.event[eventName];
+        event = bacon.event[eventName];
         // Subscribe cb to the stream and save unsubscribe hook
-        event._unsubscribers[cb] = event._eventStream.onValue(function(val){
+        event._unsubscribers[cb] = event._eventStream.onValue(function (val) {
             cb(val);
         });
     },
     unsubscribe: function (eventName, cb) {
         var bacon = this.bacon;
         if (typeof bacon.event[eventName] === 'undefined') {
-            console.warn('Event is undefined in unsubscribe method');
-        }
-        else if (typeof bacon.event[eventName]._unsubscribers[cb] !== 'function') {
-            console.warn('cb is undefined in unsubscribe method');
-        }
-        else {
+//            console.warn('Event is undefined in unsubscribe method');
+        } else if (typeof bacon.event[eventName]._unsubscribers[cb] !== 'function') {
+//            console.warn('cb is undefined in unsubscribe method');
+        } else {
             // call of unsubscribe function
             this.bacon.event[eventName]._unsubscribers[cb]();
         }
     }
     // We dont need publish function because we use Bacon.fromEvent(this,'publish'...)
     // to connect directly to the EventEmitter
-    //publish: function(eventName, value){}
-}
+    // publish: function(eventName, value){}
+};
--- plugins/simple.js
+++ plugins/simple.js
@@ -1,65 +1,65 @@
 module.exports = {
 
-    init: function(mediator){
-        if (typeof mediator["_innerTransport"] === 'undefined') {
-            mediator["_innerTransport"] = {};
+    init: function (mediator) {
+        if (typeof mediator._innerTransport === 'undefined') {
+            mediator._innerTransport = {};
         }
     },
     subscribe: function (eventName, cb) {
-        var mediator = this;
-        var self = this._innerTransport;
+        var self = this;
+        var transport = this._innerTransport;
 
         // If we have this event then subscribe
-        if (typeof self[eventName] !== "undefined") {
-            self[eventName].subscribers.push(cb);
+        if (typeof transport[eventName] !== 'undefined') {
+            transport[eventName].subscribers.push(cb);
         } else {
             // create new event
-            self[eventName] =  {subscribers: [cb]};
-            mediator.emit('addChannel',eventName);
+            transport[eventName] = {subscribers: [cb]};
+            self.emit('addChannel', eventName);
         }
-        return self[eventName];
+        return transport[eventName];
     },
     unsubscribe: function (eventName, cb) {
-        var mediator = this;
-        var self = this._innerTransport;
+        var self = this,
+            transport = this._innerTransport,
+            subscribers, idxOf;
 
-        if (typeof self[eventName] !== 'undefined' && typeof self[eventName].subscribers !== 'undefined') {
+        if (typeof transport[eventName] !== 'undefined'
+            && typeof transport[eventName].subscribers !== 'undefined') {
             // delete cb from subscribers
-            var subscribers = self[eventName].subscribers,
-                idxOf = subscribers.indexOf(cb);
+            subscribers = transport[eventName].subscribers;
+            idxOf = subscribers.indexOf(cb);
             while (idxOf !== -1) {
                 subscribers.splice(idxOf, 1);
                 idxOf = subscribers.indexOf(cb);
             }
             // delete channel if no subscribers
             if (subscribers.length === 0) {
-                delete self[eventName];
-                mediator.emit('deleteChannel',eventName);
+                delete transport[eventName];
+                self.emit('deleteChannel', eventName);
             }
         }
     },
-    publish: function(eventName, value){
-        var self = this._innerTransport;
+    publish: function (eventName, value) {
+        var transport = this._innerTransport,
+            subscribers;
 
-        if (self[eventName] === undefined) {
-            console.warn("No subscribers for this publish");
-            //self[eventName] = {"subscribers": []};
-        }
-        else {
-            var subscribers = self[eventName].subscribers;
+        if (typeof transport[eventName] === 'undefined') {
+//            console.warn("No subscribers for this publish");
+//            self[eventName] = {"subscribers": []};
+        } else {
+            subscribers = transport[eventName].subscribers;
             // Check if subscribers is an Array
             if (Object.prototype.toString.call(subscribers) !== '[object Array]') {
-                throw new Error("Subscribers property of event mast be an Array.");
+                throw new Error('Subscribers property of event mast be an Array.');
             }
             // IE: 9+
-            subscribers.forEach(function(cb/*,ind,arr*/){
-                if (typeof cb !== "function") {
-                    throw new Error("Subscriber is not a function.");
+            subscribers.forEach(function (cb) {
+                if (typeof cb !== 'function') {
+                    throw new Error('Subscriber is not a function.');
                 }
-                cb.call(self,value);
+                cb.call(transport, value);
             });
-
         }
     }
-
 };
